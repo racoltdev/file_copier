939176736f45a9c507bbbdc87e36f378c1a18566
racoltdev <racoltdev@gmail.com>
2024-12-29 14:48:11 -0500

    Works when dirs or files have spaces

--- file_copier.sh
+++ file_copier.sh
@@ -1,7 +1,27 @@
 #! /usr/bin/bash
 
+function getLengthOfDirPart {
+	file="$1"
+
+	# Sub spaces for some invalid char
+	noSpaces="$(echo "$file" | tr " " ";")"
+
+	# Separate the $file string by "/" delimeter
+	IFS="/" read -ra split_name <<< "$noSpaces"
+
+	# Remove last item to get the dir name
+	# [@] means act on all elements of the array
+	# #split_name[@] is the number of items in split_name
+	dir_parts="${split_name[@]::${#split_name[@]}-1}"
+
+	# Sub " " separators back for "/"
+	# This treats the whole thing as a string again so it's easier to get length
+	whole_dir=$(echo "${dir_parts[@]}" | tr " " "/")
+
+	return ${#whole_dir}
+}
+
 required_args=2
-#exclude_dirs="-type f"
 exclude_dirs="-type d ( -wholename /dev/null"
 verbose=false
 src=""
@@ -19,9 +39,9 @@ while [ $# -gt 0 ]; do
 			;;
 		*)
 			if [[ -z  "$src" ]]; then
-				src=$1
+				src="$1"
 			elif [[ -z "$dest" ]] then
-				dest=$1
+				dest="$1"
 			else
 				echo "Too many arguments!"
 				exit
@@ -31,24 +51,18 @@ while [ $# -gt 0 ]; do
 	esac
 done
 exclude_dirs+=" ) -prune"
-files="$(find "$src" $exclude_dirs -o -type f -print)"
 
-for file in $files; do
+find "$src" $exclude_dirs -o -type f -print0 | while IFS= read -r -d '' file; do
 	# Remove first two characters "./"
 	file=${file:2}
+
 	if [ $verbose = true ]; then
 		echo "Copying: ""$file"
 	fi
 
-	# Separate the $file string by "/" delimeter
-	IFS="/" read -ra split_name <<< "$file"
-	# Remove last item to get the dir name
-	# [@] means act on all elements of the array
-	# #split_name[@] is the number of items in split_name
-	dir="${split_name[@]::${#split_name[@]}-1}"
-
-	# Sub " " separators back for "/"
-	dir=$(echo $dir | tr " " "/")
+	getLengthOfDirPart "$file"
+	dir_length=$?
+	dir=${file::$dir_length}
 
 	if [ -n "$dir" ]; then
 		mkdir -p "$dest""$dir"  && \
